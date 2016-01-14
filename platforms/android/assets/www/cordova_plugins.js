dc0c2a80f3e9b7655f96151b6f84da41ee35f0c4
Aleksandr Katerinich <panik.adm@gmail.com>
2016-01-14 23:47:35 +0200

    Added footer buttom

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -5,12 +5,21 @@ module.exports = [
         "id": "cordova-plugin-whitelist.whitelist",
         "pluginId": "cordova-plugin-whitelist",
         "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-imagepicker/www/imagepicker.js",
+        "id": "cordova-plugin-imagepicker.ImagePicker",
+        "pluginId": "cordova-plugin-imagepicker",
+        "clobbers": [
+            "plugins.imagePicker"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.2.0"
+    "cordova-plugin-whitelist": "1.2.0",
+    "cordova-plugin-imagepicker": "1.1.0"
 }
 // BOTTOM OF METADATA
 });
